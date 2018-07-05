283eac996f97c2173a3a0cfc92c5d6aad268b90e
David Bucklin <david.bucklin@dcr.virginia.gov>
2018-07-05 17:22:22 -0400

    update raster aquatic areas, change WD

--- CreateAquaAccessPolys.py
+++ CreateAquaAccessPolys.py
@@ -15,7 +15,7 @@ import Helper
 from Helper import *
 from arcpy import env
 
-def RasterizeAll(listFeat = [], accessPts = '', out = '', template = '', value = 1, erase = "#"):
+def RasterizeAll(listFeat, accessPts, out, template, value = 1, erase = "#"):
    '''Rasterizes all feature classes in list into one raster layer,
    with the value assigned in 'value'''
    
@@ -24,14 +24,12 @@ def RasterizeAll(listFeat = [], accessPts = '', out = '', template = '', value =
    arcpy.env.cellSize = template
    arcpy.env.outputCoordinateSystem = template
    arcpy.env.overwriteOutput = True
-   
-   sgdb = arcpy.env.scratchGDB
 
    list_rast = list()
    # add a rastval field
    for l in listFeat:
       printMsg('Rasterizing file ' + l + '...')
-      nm = sgdb + os.sep + os.path.basename(l).replace('.shp','') + '_rast'
+      nm = os.path.basename(l).replace('.shp','') + '_rast'
       arcpy.AddField_management(l, 'rastval', 'DOUBLE')
       arcpy.CalculateField_management (l, 'rastval', value, 'PYTHON')
       if arcpy.Describe(l).Name == "nhd_area_canalditch":
@@ -46,8 +44,8 @@ def RasterizeAll(listFeat = [], accessPts = '', out = '', template = '', value =
    
    # erase features if given
    if erase != "#":
-      printMsg('Rasterizing file ' + erase + '...')
-      erase_rast = sgdb + os.sep + os.path.basename(erase).replace('.shp','') + '_rast'
+      printMsg('Rasterizing erase features ' + erase + '...')
+      erase_rast = os.path.basename(erase).replace('.shp','') + '_rast'
       arcpy.AddField_management(erase, 'rastval', 'DOUBLE')
       arcpy.CalculateField_management (erase, 'rastval', 1, 'PYTHON')
       arcpy.FeatureToRaster_conversion (erase, 'rastval', erase_rast, template)
@@ -62,7 +60,7 @@ def RasterizeAll(listFeat = [], accessPts = '', out = '', template = '', value =
       
    # add access points
    printMsg('Rasterizing file ' + accessPts + '...')
-   ap = sgdb + os.sep + os.path.basename(accessPts).replace('.shp','') + '_rast'
+   ap = os.path.basename(accessPts).replace('.shp','') + '_rast'
    arcpy.AddField_management(accessPts, 'rastval', 'DOUBLE')
    arcpy.CalculateField_management (accessPts, 'rastval', value, 'PYTHON')
    arcpy.FeatureToRaster_conversion (accessPts, 'rastval', ap, template)
@@ -81,49 +79,66 @@ def RasterizeAll(listFeat = [], accessPts = '', out = '', template = '', value =
       
    return out_rast
 
-def CreateAccessPolys(feat, costSurf, out, maxCost = 5000):
+def CreateAccessPolys(accessPts, costSurf, outPolys, outPts, maxCost = 5000):
    
    arcpy.env.extent = costSurf
    arcpy.env.snapRaster = costSurf
    arcpy.env.cellSize = costSurf
    arcpy.env.outputCoordinateSystem = costSurf
    arcpy.env.overwriteOutput = True
-   
-   # did this in arcpro since it was one time and a fast process
+
    printMsg("Running cost distance...")
-   cd = CostDistance(feat, costSurf, maxCost)
+   cd = CostDistance(accessPts, costSurf, maxCost)
    cd1 = Con(cd, 1, "", "Value >= 0")
+   cd1 = RegionGroup(cd1, "EIGHT")
    
    printMsg("Converting to polygons...")
-   # region group
-   fc1 = arcpy.RasterToPolygon_conversion (cd1, out, "NO_SIMPLIFY")
-   # dissolve 
+   fc1 = arcpy.RasterToPolygon_conversion (cd1, "r2p", "NO_SIMPLIFY")
+   fc2 = arcpy.Dissolve_management(fc1, outPolys, "gridcode") # creates one polygon per gridcode
    
-   # garbagePickup([cd, cd1])
-   return fc1
+   # join polygon id/area to points
+   arcpy.AddField_management(fc2, "area_ha", "FLOAT")
+   arcpy.CalculateField_management(fc2, "area_ha", '!shape.area@hectares!',"PYTHON_9.3")
+   arcpy.SpatialJoin_analysis(accessPts, fc2, outPts)
+   
+   garbagePickup([cd, cd1, fc1])
+   return fc2
 
 def main():
-   # Set up variables
+   
+   # set workspace for intermediate outputs
+   arcpy.env.workspace = r'C:\David\scratch\scratch_gdb.gdb'
+   
+   # create aquatic cost surface raster from aquatic features
+   # list of feature classes to rasterize
    listFeat = [r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\nhd_area_wtrb',
             r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\pub_fish_lake1',
             r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\nhd_flowline1',
-            r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\nhd_flowline_canalditch',
             r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\public_beaches_polys',
-            r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\nhd_area_canalditch']
+            r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\nhd_area_canalditch'] # these are now included in nhd_area_wtrb; but need to be handled differently for burn-in
    # included digitized beaches, subsets of NHD waterbody, area, and flowline
+   # access pts (use select to exclude use = 0 points)
+   arcpy.Select_analysis(r'C:\David\projects\rec_model\source_database\exports\access_a.shp', r'C:\David\projects\rec_model\source_database\exports\access_a_noZero.shp',"use in (1,2)")
    accessPts = r'C:\David\projects\rec_model\source_database\exports\access_a_noZero.shp'
+   # template raster
    template = r'C:\David\projects\va_cost_surface\cost_surfaces\costSurf_all.tif'
+   # output raster
    out = r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\aqua_rast_nodam'
    value = 1
+   # erase features (will exclude these rasterized features from raster)
    erase = r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\nhd_line_damweir'
+   # run fn
    RasterizeAll(listFeat, accessPts, out, template, value, erase)
    
-   # Specify function to run
-   # costSurf = RasterizeAll(listFeat, out, template)
+   ### generate aquatic areas
+   import datetime
+   dt = str(datetime.date.today()).replace("-","_")
+   accessPts = r'C:\David\projects\rec_model\source_database\exports\access_a_noZero.shp'
    costSurf = r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\aqua_rast_nodam'
-   feat = r'C:\David\projects\rec_model\source_database\exports\access_a.shp'
-   out = r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\aqua_accesspolys_test'
-   aqPolys = CreateAccessPolys(feat, costSurf, out, maxCost = 5000)
+   outPolys = r'C:\David\projects\rec_model\rec_datasets\derived\terr_aqua_polygon_processing.gdb\aqua_accesspolys_' + dt
+   outPts = r'C:\David\projects\rec_model\source_database\exports\access_a_wAreas_' + dt + '.shp'
+   # run fn
+   CreateAccessPolys(accessPts, costSurf, outPolys, outPts, maxCost = 5000)
 
-#if __name__ == '__main__':
-#   main()
+if __name__ == '__main__':
+   main()
